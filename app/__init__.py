79817e4e7a87f15cbdcd136f9c296c6630f7f1d3
vinicius <viniciuspimentl@gmail.com>
2026-04-26 10:22:54 -0300

    feat: criando conexão com o mongodb atraves da string de conexão

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from pymongo import MongoClient
 
-from .routes.main import main_bp
+db = None
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object("config.Config")
+    global db
+
+    try:
+        client = MongoClient(app.config["MONGO_URI"])
+        db = client.get_default_database()
+        print("Conexão com o MongoDB estabelecida com sucesso!")
+    except Exception as e:
+        print(f"Erro na conexão com o banco de dados: {e}")
+
+    from .routes.main import main_bp
+
     app.register_blueprint(main_bp)
     return app
