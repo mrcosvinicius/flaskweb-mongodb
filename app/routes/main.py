9d0a3882d1c30e0b336e5964184bba412c95e3c2
vinicius <viniciuspimentl@gmail.com>
2026-04-30 21:32:31 -0300

    feat: adicionado funcionalidades de criação, deleção, atualização e autenticação jwt

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,9 +1,14 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
 from bson import ObjectId
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, current_app, jsonify, request
 from pydantic import ValidationError
 
 from app import db
-from app.models.usuarios import LoginUsuario
+from app.decorators import token_obrigatorio
+from app.models.clientes import AtualizaCliente, Cliente, ClienteDBModel
+from app.models.usuarios import Usuario
 
 main_bp = Blueprint("main_bp", __name__)
 
@@ -13,7 +18,7 @@ main_bp = Blueprint("main_bp", __name__)
 def login():
     try:
         dados = request.get_json()
-        dados_usuarios = LoginUsuario(**dados)
+        dados_usuarios = Usuario(**dados)
 
     except ValidationError as e:
         return jsonify(
@@ -23,19 +28,31 @@ def login():
         return jsonify(
             {"error": "Erro ao processar a solicitação", "details": str(e)}
         ), 500
-    if dados_usuarios.nome == "admin" and dados_usuarios.senha == "admin":
-        return jsonify(
-            {"message": f"Usuario {dados_usuarios.nome} autenticado com sucesso!"}
+    if (
+        dados_usuarios.nome == "admin"
+        and dados_usuarios.senha == current_app.config["SECRET_KEY"]
+    ):
+        token = jwt.encode(
+            {
+                "user_id": dados_usuarios.nome,
+                "exp": datetime.now(timezone.utc) + timedelta(hours=2),
+            },
+            current_app.config["SECRET_KEY"],
+            algorithm="HS256",
         )
-    else:
-        return jsonify({"error": "Credenciais inválidas"}), 401
+        return jsonify({"token de acesso": token}), 200
+
+    return jsonify({"error": "Credenciais inválidas"}), 401
 
 
 # rf: o sistema deve permitir listagem de todos os clientes
 @main_bp.route("/clientes", methods=["GET"])
 def listar_clientes():
     todos_clientes = db.clientes.find({})
-    clientes_list = []
+    clientes_list = [
+        ClienteDBModel(**cliente).model_dump(by_alias=True, exclude_none=True)
+        for cliente in todos_clientes
+    ]
 
     for cliente in todos_clientes:
         cliente["_id"] = str(cliente["_id"])
@@ -54,28 +71,77 @@ def visualizar_cliente(cliente_id):
 
     cliente = db.clientes.find_one({"_id": uid})
     if cliente:
-        cliente["_id"] = str(cliente["_id"])
-        return jsonify(cliente)
+        cliente_model = ClienteDBModel(**cliente).model_dump(
+            by_alias=True, exclude_none=True
+        )
+        return jsonify(cliente_model)
     else:
         return jsonify({"error": "Cliente não encontrado"}), 404
 
 
 # rf: o sistema deve permitir a criacao de um novo cliente
 @main_bp.route("/clientes", methods=["POST"])
-def criar_cliente():
-    return jsonify({"message": "Rota para criar um novo cliente"})
+@token_obrigatorio
+def criar_cliente(token_dados):
+    try:
+        cliente = Cliente(**request.get_json())
+    except ValidationError as e:
+        return jsonify(
+            {"error": "Dados de cliente inválidos", "details": e.errors()}
+        ), 400
+    except Exception as e:
+        return jsonify(
+            {"error": "Erro ao processar a solicitação", "details": str(e)}
+        ), 500
+    db.clientes.insert_one(cliente.model_dump(by_alias=True, exclude_none=True))
+    return jsonify({"message": f"Cliente {cliente.nome} criado com sucesso!"}), 201
 
 
 # rf: o sistema deve permitir a atualizacao de um unico cliente e cliente existente
-@main_bp.route("/clientes/<int:cliente_id>", methods=["PUT"])
-def atualizar_cliente(cliente_id):
-    return jsonify({"message": f"Rota para atualizar o cliente {cliente_id}"})
+@main_bp.route("/clientes/<string:cliente_id>", methods=["PUT"])
+@token_obrigatorio
+def atualizar_cliente(token_dados, cliente_id):
+    try:
+        uid = ObjectId(cliente_id)
+        dados_atualizado = AtualizaCliente(**request.get_json())
+
+    except ValidationError as e:
+        return jsonify({"error": e.errors()}), 500
+
+    resultado_atualizado = db.clientes.update_one(
+        {"_id": uid},
+        {"$set": dados_atualizado.model_dump(exclude_unset=True)},
+    )
+
+    if resultado_atualizado.matched_count == 0:
+        return jsonify({"error": "Cliente não encontrado"}), 404
+
+    cliente_atualizado = db.clientes.find_one({"_id": uid})
+
+    if cliente_atualizado:
+        cliente_model = ClienteDBModel(**cliente_atualizado).model_dump(
+            by_alias=True, exclude_none=True
+        )
+        return jsonify(cliente_model)
+    else:
+        return jsonify({"error": "Cliente não encontrado após atualização"}), 404
 
 
 # rf: o sistema deve permitir a delecao de um unico cliente e cliente existente
-@main_bp.route("/clientes/<int:cliente_id>", methods=["DELETE"])
-def deletar_cliente(cliente_id):
-    return jsonify({"message": f"Rota para deletar o cliente {cliente_id}"})
+@main_bp.route("/clientes/<string:cliente_id>", methods=["DELETE"])
+@token_obrigatorio
+def deletar_cliente(token_dados, cliente_id):
+    try:
+        uid = ObjectId(cliente_id)
+        produto_deletado = db.clientes.delete_one({"_id": uid})
+
+        if produto_deletado.deleted_count == 0:
+            return jsonify({"error": "Cliente não encontrado"}), 404
+
+    except ValidationError as e:
+        return jsonify({"error": e.errors()}), 500
+
+    return jsonify({"message": f"Cliente {cliente_id} deletado com sucesso!"}), 204
 
 
 # rf: o sistema deve permitir a importacao de compras através de um arquivo
