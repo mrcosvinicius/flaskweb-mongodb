ca81beb73db29f1c6c0b0b10ac175acf647d7ca6
vinicius <viniciuspimentl@gmail.com>
2026-04-30 21:21:03 -0300

    feat criando classe para evitar expor a senha na resposta

--- app/models/usuarios.py
+++ app/models/usuarios.py
@@ -1,6 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
-class LoginUsuario(BaseModel):
+class Usuario(BaseModel):
     nome: str
     senha: str
+
+
+class UsuarioResposta(BaseModel):
+    id: str = Field(alias="_id")
+    nome: str
